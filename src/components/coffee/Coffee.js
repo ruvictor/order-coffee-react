94152c0977f1b58c9682384774da2cef9b397a29
Victor <rusuvictor30@gmail.com>
2021-02-18 23:06:46 -0500

    Click size

--- src/components/coffee/Coffee.js
+++ src/components/coffee/Coffee.js
@@ -1,3 +1,4 @@
+import React, { Component } from 'react';
 import styled from 'styled-components';
 import './Coffee.css';
 
@@ -14,35 +15,48 @@ const Vapour10 = styled.span`--v:10;`;
 const Vapour11 = styled.span`--v:11;`;
 const Vapour12 = styled.span`--v:12;`;
 
-const Coffee = (props) => {
-    let style = {
-        height: props.size === 'small' ? '200px' : 
-        props.size === 'medium' ? '250px' : ''
-    };
+class Coffee extends Component{
+
+    state = {
+        clicked: ''
+    }
     
-    return (
-        <div className="coffeeBlock">
-            <div className="coffee">
-                <div className="vapour">
-                    <Vapour1></Vapour1>
-                    <Vapour2></Vapour2>
-                    <Vapour5></Vapour5>
-                    <Vapour4></Vapour4>
-                    <Vapour6></Vapour6>
-                    <Vapour3></Vapour3>
-                    <Vapour7></Vapour7>
-                    <Vapour8></Vapour8>
-                    <Vapour9></Vapour9>
-                    <Vapour10></Vapour10>
-                    <Vapour11></Vapour11>
-                    <Vapour12></Vapour12>
+
+    chooseCoffee = (size) => {
+        console.log(size);
+        this.setState({clicked: size})
+        // return size;
+    }
+    
+    render(){
+        let style = {
+            height: this.props.size === 'small' ? '200px' : 
+            this.props.size === 'medium' ? '250px' : ''
+        };
+        return (
+            <div className="coffeeBlock" onClick={() => this.chooseCoffee(this.props.size)}>
+                <div className="coffee">
+                    <div className="vapour">
+                        <Vapour1></Vapour1>
+                        <Vapour2></Vapour2>
+                        <Vapour5></Vapour5>
+                        <Vapour4></Vapour4>
+                        <Vapour6></Vapour6>
+                        <Vapour3></Vapour3>
+                        <Vapour7></Vapour7>
+                        <Vapour8></Vapour8>
+                        <Vapour9></Vapour9>
+                        <Vapour10></Vapour10>
+                        <Vapour11></Vapour11>
+                        <Vapour12></Vapour12>
+                    </div>
                 </div>
+                <div className="lid"></div>
+                <div className="cup" style={style}></div>
+                <div className="bottom"></div>
             </div>
-            <div className="lid"></div>
-            <div className="cup" style={style}></div>
-            <div className="bottom"></div>
-        </div>
-    );
+        )
+    }
 }
 
 export default Coffee;
--- src/components/sizes/Size.js
+++ src/components/sizes/Size.js
@@ -1,16 +1,12 @@
 import Coffee from '../coffee/Coffee';
 import './Sizes.css';
 
-const chooseSize = (size) => {
-    console.log(size);
-}
-
 const Size = () => {
     return (
         <>
             <div className="StepTitle">Choose your Size</div>
             <div className="Sizes">
-                <Coffee onClick={() => chooseSize('small')} size="small" />
+                <Coffee size="small" />
                 <Coffee size="medium" />
                 <Coffee size="large" />
             </div>
